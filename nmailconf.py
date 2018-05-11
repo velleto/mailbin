6cc6d23fec91bf8d6c038c8fcdd58427d2af72e1
velleto <git@velleto.com>
2018-05-12 00:41:34 +0200

    Adding files.

---
+++ nmailconf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Example config
+
+mailroot	= "/home/username/mailroot"
+histfile	= "/home/username/mailroot/bin/metadata/notification_history.pkl"
+
+mboxname	= "mb"
+
+header		= "New mail"
+
+accounts	= {
+				"personal@domain.tld": 0,
+				"work@company.tld": 0,
+			}
